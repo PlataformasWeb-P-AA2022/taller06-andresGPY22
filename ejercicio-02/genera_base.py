c9dd0343689e744e9bd37b1740c1e2ba88507f6f
Andres Jimenez <aajimenez14@utl.edu.ec>
2022-05-17 20:49:14 -0500

    Taller

---
+++ ejercicio-02/genera_base.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine
+
+# se genera en enlace al gestor de base de
+# datos
+# para el ejemplo se usa la base de datos
+# sqlite
+engine = create_engine('sqlite:///country.db')
+
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+
+from sqlalchemy import Column, Integer, String
+
+class Cosax(Base):
+    __tablename__ = 'laspersonas'
+    
+    id = Column(Integer, primary_key=True)
+    nombrepais = Column(String)
+    capital = Column(String)
+    continente = Column(String)
+    dial = Column(String)
+    geoname = Column(Integer)
+    itu = Column(String)
+    lenguajes = Column(String)
+    dependiente = Column(String)
+
+Base.metadata.create_all(engine)
